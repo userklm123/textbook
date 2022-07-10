a1c62c7464e7f2d7aca1c84e3bfe2b53791a5dce
userklm123 <1209410810@qq.com>
2022-07-10 13:56:47 +0800

    7/10更新

--- Vue/src/page/departmentHome.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<h2>系部面板</h2>
-</template>
-
-<script>
-</script>
-
-<style>
-</style>
--- Vue/src/page/teacherHome.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<h2>老师面板!</h2>
-</template>
-
-<script>
-</script>
-
-<style>
-</style>
